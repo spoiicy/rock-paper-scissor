c691e7912c256cd6b5d01511e1a5f9630a47024d
spoiicy <akshitmaheshwary@gmail.com>
2023-01-26 22:15:59 +0530

    finished making rock paper scissors game

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function getComputerChoice(){
             selection = 'rock';
             break;
         case 2:
-            selection = 'scissor';
+            selection = 'scissors';
             break;
         case 3:
             selection = 'paper';
@@ -16,51 +16,134 @@ function getComputerChoice(){
 }
 
 
-function playRound(playerSelection, computerSelection){
 
-    playerSelection = prompt("Rock, Paper or Scissor?").toLowerCase();
-    computerSelection = getComputerChoice();
+function playRound(playerSelection){
+
+    let computerSelection = getComputerChoice();
+
+    let computerHand = document.querySelector('#computer-hand');
+    computerHand.innerHTML = `<img src=\'${computerSelection}.png\'>`
     
 
-    if((playerSelection == 'rock' && computerSelection == 'scissor') || (playerSelection == 'paper' && computerSelection =='rock') || (playerSelection == 'scissor' && computerSelection == 'paper')){
+    if((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection =='rock') || (playerSelection == 'scissors' && computerSelection == 'paper')){
         return 1;
     }
     else if (playerSelection == computerSelection){
+        
         return 0;
     }
     else{
+        
         return -1;
     }
 }
 
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    for(let i = 0;i<5;i++){
-        let result = playRound();
-        if(result == 1){
-            ++playerScore;
-        }
-        else if(result == -1){
-            ++computerScore;
-        }
-        else{
-            continue;
-        }
+
+function score(result){
+    if(result == 1){
+        ++playerScore;
+        playerScore_div.textContent = "Player Score : " + playerScore;
         
     }
-    console.log('player score is ' + playerScore);
-    console.log('computer score is ' + computerScore);
-    if(playerScore == computerScore){
-        console.log('it\'s a tie');
+    if(result == -1){
+        ++computerScore;
+        computerScore_div.textContent ="Computer Score : " + computerScore;
+        
+
     }
-    else if(playerScore > computerScore){
-        console.log('you won');
-    }
-    else{
-        console.log('computer won');
+
+    if(playerScore == '5' || computerScore == '5'){
+        if(playerScore == 5){
+            const player_choice = document.querySelector('.player-choice');
+            player_choice.classList.toggle('winner');
+            player_choice.innerHTML = '<div id = \"play-again\"><p>You Won</p><button type = \"button\" onclick = \"window.location.reload()\">Play Again</button></div>';
+        }
+        else{
+            
+            const player_choice = document.querySelector('.player-choice');
+            player_choice.classList.toggle('winner');
+            player_choice.innerHTML = '<div id = \"play-again\"><p>Computer Won</p><button type = \"button\" onclick = \"window.location.reload()\">Play Again</button></div>';
+        }
     }
 }
 
 
-game();
+
+
+const playerHand = document.querySelector('#player-hand');
+
+let computerHand = document.querySelector('#computer-hand');
+
+const playerRock = document.createElement('img');
+const playerPaper = document.createElement('img');
+const playerScissors = document.createElement('img');
+
+
+const computerRock = document.createElement('img');
+
+playerRock.src = 'rock.png';
+playerPaper.src = 'hand.png';
+playerScissors.src = 'scissors.png';
+
+
+
+const rockButton = document.querySelector('#rock');
+    rockButton.addEventListener('click',()=>{
+        playerHand.innerHTML ='<img src=\'rock.png\'>'
+        //playerHand.removeChild();
+        //playerHand.textContent = 'rock';
+        let result = playRound('rock');
+        score(result);
+        //playerHand.textContent = '';
+        //computerHand.textContent = '';
+    }
+)    
+
+
+
+
+const paperButton = document.querySelector('#paper');
+    paperButton.addEventListener('click',()=>{         
+        playerHand.innerHTML ='<img src=\'paper.png\'>'
+        //playerHand.appendChild(playerPaper);
+        //playerHand.removeChild();
+        //playerHand.textContent = 'paper';
+        let result = playRound('paper');
+        score(result);
+        //playerHand.textContent = '';
+    }
+)
+
+const scissorsButton = document.querySelector('#scissors');
+    scissorsButton.addEventListener('click',()=>{
+        playerHand.innerHTML ='<img src=\'scissors.png\'>'
+        //playerHand.appendChild(playerScissors);
+        //playerHand.textContent = 'scissors';
+        result = playRound('scissors');
+        score(result);
+        //playerHand.textContent = '';
+    }
+
+)
+
+let playerScore = 0;
+let computerScore = 0;
+
+let playerScore_div = document.querySelector('#player-score');
+let computerScore_div = document.querySelector('#computer-score');
+
+
+console.log(playerScore);
+console.log(computerScore);
+
+
+
+    
+    
+
+
+
+
+
+
+
